df62f7d6ae3acf1b0180691784847058d555a931
EliasSabja <elias.sabja@uc.cl>
2020-05-12 22:31:14 -0400

    PErsonalizacion tablas 3

--- Sites/vistas/vista_artistas.php
+++ Sites/vistas/vista_artistas.php
@@ -7,22 +7,22 @@
     $result -> execute();
     $dataCollected = $result -> fetchAll();
 ?>
-
-<section class="section section-destination">
-    <div class="container">
-        <table>
-        <tr>
-            <th>Nombre artista</th>
-            <th>Ver detalles</th>
-        </tr>
-        <?php
-            foreach ($dataCollected as $p) {
-                echo "<tr> <td>$p[0]</td> <td> <a> Ver detalles </a> </td> </tr>";
-            }
-        ?>
-        </table>
-        <hr />
-    </div>
-</section>
-
+<div class="content-box">
+    <section class="section section-destination">
+        <div>
+            <table>
+            <tr>
+                <th>Nombre artista</th>
+                <th>Ver detalles</th>
+            </tr>
+            <?php
+                foreach ($dataCollected as $p) {
+                    echo "<tr> <td>$p[0]</td> <td> <a> Ver detalles </a> </td> </tr>";
+                }
+            ?>
+            </table>
+            <hr />
+        </div>
+    </section>
+</div>
 <?php include('../templates/footer.html'); ?>
