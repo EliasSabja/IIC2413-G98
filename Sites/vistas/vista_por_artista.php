8ab061e4320f38616782dc27d043d91fb3f35082
EliasSabja <elias.sabja@uc.cl>
2020-05-15 21:16:57 -0400

    vista_por_artista implementada

--- Sites/vistas/vista_por_artista.php
+++ Sites/vistas/vista_por_artista.php
@@ -1,6 +1,42 @@
 <?php include('../templates/header.html'); ?>
-<?php $current_aid = $_GET['id'];?>
-<h1> <?php 
-echo "$current_aid"
-?></h1>
+
+<?php
+    $current_aid = $_GET['id'];
+
+    require("../assets/conexion.php");
+    $query = "SELECT DISTINCT * FROM artistas WHERE artistas.aid=$current_aid;";
+    $result = $db8 -> prepare($query);
+    $result -> execute();
+    $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
+?>
+
+<section class="section section-destination">
+    <div class="section-title">
+        <div class="container">
+            <?php echo "<h1 class='title'> $dataCollected[0] </h1>"?>
+            <h3>Datos del artista</h3>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <article>
+            <table class="custom">
+            <tr>
+                <th>Nombre artista</th>
+                <th>Fecha de nacimiento</th>
+                <th>Fecha de fallecimiento</th>
+                <th>Descripción</th>
+            </tr>
+            <?php
+                foreach ($dataCollected as $p) {
+                    echo "<tr> <td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td> </tr>";
+                }
+            ?>
+            </table>
+            </article>
+            <hr />
+        </div>
+    </div>
+</section>
+
 <?php include('../templates/footer.html'); ?>
