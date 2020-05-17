6fa1d20665cc55a4cacb68599664d926c85ccaab
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-17 00:26:09 -0400

    test

--- Sites/vistas/session/controlador_login.php
+++ Sites/vistas/session/controlador_login.php
@@ -6,20 +6,16 @@
     $data = $result -> fetchAll();
     $credenciales =  array(trim($_POST["correo"]), $_POST["contrasena"]);
     #$valores = array(gettype($credenciales[1]), gettype($data[0][1]));
-    #echo "<!DOCTYPE html> <body> <h1> $valores[0], $valores[1] </h1>";
-    #$validado = false;
-    #foreach ($data as $tupla){
-    #    if (trim($_POST["correo"]) == trim($tupla[0])){
-    #        echo 1;
-    #        $validado = true;
-    #    }
-    #    if ($_POST["contrasena"] == $tupla[1]){
-    #        echo 2;
-    #        $validado = true;
-    #    }
-    #}
-    #echo $validado ? 'true':'false';
-    #echo "</body></html>" ;
+    echo "<!DOCTYPE html> <body>";
+    $validado = false;
+    foreach ($data as $tupla){
+        if ((trim($_POST["correo"]) == trim($tupla[0])) and ($_POST["contrasena"] == $tupla[1])){
+            echo 1;
+            $validado = true;
+        }
+    }
+    echo $validado ? 'true':'false';
+    echo "</body></html>" ;
     if (in_array($credenciales, $data)) {
         session_start();
         $_SESSION["user"] = $credenciales[0];
