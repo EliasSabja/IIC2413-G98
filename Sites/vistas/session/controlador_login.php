f323e5a96363df7e3771ca277e599b03763b46a6
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-17 00:28:50 -0400

    test

--- Sites/vistas/session/controlador_login.php
+++ Sites/vistas/session/controlador_login.php
@@ -4,19 +4,18 @@
     $result = $db9 -> prepare($query);
     $result -> execute();
     $data = $result -> fetchAll();
-    $credenciales =  array(trim($_POST["correo"]), $_POST["contrasena"]);
+    $credenciales =  array($_POST["correo"], $_POST["contrasena"]);
     #$valores = array(gettype($credenciales[1]), gettype($data[0][1]));
-    echo "<!DOCTYPE html> <body>";
+    #echo "<!DOCTYPE html> <body>";
     $validado = false;
     foreach ($data as $tupla){
         if ((trim($_POST["correo"]) == trim($tupla[0])) and ($_POST["contrasena"] == $tupla[1])){
-            echo 1;
             $validado = true;
         }
     }
-    echo $validado ? 'true':'false';
-    echo "</body></html>" ;
-    if (in_array($credenciales, $data)) {
+    #echo $validado ? 'true':'false';
+    #echo "</body></html>" ;
+    if ($validado) {
         session_start();
         $_SESSION["user"] = $credenciales[0];
         header("Location: ../../index.php");
