6480375a578ef2021a70a10d44bd4b5326a7aceb
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-16 23:55:44 -0400

    test

--- Sites/vistas/session/controlador_login.php
+++ Sites/vistas/session/controlador_login.php
@@ -5,11 +5,12 @@
     $result -> execute();
     $data = $result -> fetchAll();
     $credenciales =  array($_POST["correo"], $_POST["contrasena"]);
-    #echo "<!DOCTYPE html> <body> <h1> $data[0], $data[1] </h1>";
+    $valores = array(gettype($credenciales[1]), gettype($data[0][1]));
+    echo "<!DOCTYPE html> <body> <h1> $valores[0], $valores[1] </h1>";
     #foreach($data as $p){
     #    echo "$p[0], $p[1]-";
     #}
-    #echo "</body></html>" ;
+    echo "</body></html>" ;
     if (in_array($credenciales, $data)) {
         session_start();
         $_SESSION["user"] = $credenciales[0];
