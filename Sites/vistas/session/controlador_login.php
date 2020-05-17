67d5bed61a74c641a5ac5e1630553b0de524a74a
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-16 23:51:45 -0400

    test

--- Sites/vistas/session/controlador_login.php
+++ Sites/vistas/session/controlador_login.php
@@ -1,15 +1,15 @@
 <?php
     require("../../assets/conexion.php");
-    $query = "SELECT nombre, contrasena FROM usuarios;";
+    $query = "SELECT correo, contrasena FROM usuarios;";
     $result = $db9 -> prepare($query);
     $result -> execute();
     $data = $result -> fetchAll();
     $credenciales =  array($_POST["correo"], $_POST["contrasena"]);
-    echo "<!DOCTYPE html> <body> <h1> $data[0], $data[1] </h1>";
-    foreach($data as $p){
-        echo "$p[0], $p[1]-";
-    }
-    echo "</body></html>" ;
+    #echo "<!DOCTYPE html> <body> <h1> $data[0], $data[1] </h1>";
+    #foreach($data as $p){
+    #    echo "$p[0], $p[1]-";
+    #}
+    #echo "</body></html>" ;
     if (in_array($credenciales, $data)) {
         session_start();
         $_SESSION["user"] = $credenciales[0];
