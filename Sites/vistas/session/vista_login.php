0a18a609d3b8453deca37e5aa37c40d186ed56e5
Luckbox314 <50024324+Luckbox314@users.noreply.github.com>
2020-05-17 18:52:31 -0400

    fix issue 21

--- Sites/vistas/session/vista_login.php
+++ Sites/vistas/session/vista_login.php
@@ -1,15 +1,27 @@
 <?php include('../../templates/session_header.html'); ?>
 
 
-
-<form action="controlador_login.php" method="post">
-    <label for="correo">Correo:</label>
-    <input type="text" name="correo"><br>
-    <label for="contrasena">Contraseña:</label>
-    <input type="text" name="contrasena"><br>
-    <input type="submit" value="Loguearse">
-</form>
-
+<section class="section section-destination">
+    <div class="section-title">
+        <div class="container">
+            <h2 class="title">Explora los artistas registrados</h2>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <article>
+            <form action="controlador_login.php" method="post">
+                <label for="correo">Correo:</label>
+                <input type="text" name="correo"><br>
+                <label for="contrasena">Contraseña:</label>
+                <input type="text" name="contrasena"><br>
+                <input type="submit" value="Loguearse">
+            </form>
+            </article>
+            <hr />
+        </div>
+    </div>
+</section>
 
 
 <?php include('../../templates/session_footer.html'); ?>
--- Sites/vistas/session/vista_signup.php
+++ Sites/vistas/session/vista_signup.php
@@ -1,21 +1,33 @@
 <?php include('../../templates/session_header.html'); ?>
 
 
-
-<form action="controlador_signup.php" method="post">
-    <label for="username">Username:</label>
-    <input type="text" name="username"><br>
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre"><br>
-    <label for="direccion">Direccion:</label>
-    <input type="text" name="direccion"><br>
-    <label for="correo">Correo:</label>
-    <input type="text" name="correo"><br>
-    <label for="contrasena">Contraseña:</label>
-    <input type="text" name="contrasena"><br>
-    <input type="submit" value="Loguearse">
-</form>
-
+<section class="section section-destination">
+    <div class="section-title">
+        <div class="container">
+            <h2 class="title">Explora los artistas registrados</h2>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <article>
+            <form action="controlador_signup.php" method="post">
+                <label for="username">Username:</label>
+                <input type="text" name="username"><br>
+                <label for="nombre">Nombre:</label>
+                <input type="text" name="nombre"><br>
+                <label for="direccion">Direccion:</label>
+                <input type="text" name="direccion"><br>
+                <label for="correo">Correo:</label>
+                <input type="text" name="correo"><br>
+                <label for="contrasena">Contraseña:</label>
+                <input type="text" name="contrasena"><br>
+                <input type="submit" value="Loguearse">
+            </form>
+            </article>
+            <hr />
+        </div>
+    </div>
+</section>
 
 
 <?php include('../../templates/session_footer.html'); ?>
