d13a8474ec7583346719f0e5946e7270dc20c7c9
Luckbox314 <50024324+Luckbox314@users.noreply.github.com>
2020-05-25 03:56:47 -0400

    Diseño de perfil

--- Sites/vistas/vista_perfil.php
+++ Sites/vistas/vista_perfil.php
@@ -14,32 +14,40 @@
             <h2 class="title">Explora los artistas registrados</h2>
         </div>
     </div>
-    <div class="container-fluid">
+    <div class="container" style="margin:50px;">
 
         <div class="row" style="height:200px;">
-            <div class = "col-md-8 border" style="text-align: center;" >
+            <div class = "col-md-8 " style="text-align: center;" >
                 <h1> Entradas <h1>
+                <p>Revisa todas tus Entradas a museos</p>
+                <a href="#" class="btn no-icon">Go</a>
             </div>
-            <div class = "col-md-8 border" style="text-align: center;">
+            <div class = "col-md-8 " style="text-align: center;">
                 <h1> Reservas</h1>
+                <p>Revisa todas tus reservas en hoteles</p>
+                <a href="#" class="btn no-icon">Go</a>
             </div>
-            <div class = "col-md-8 border" style="text-align: center;">
+            <div class = "col-md-8 " style="text-align: center;">
                 <h1> Tickets </h1>
+                <p>Revisa todos tus tickets para viajes</p>
+                <a href="#" class="btn no-icon">Go</a>
             </div>
         </div>
 
         <div class="row" style="height:60px;">
-            <div class = "col-md-12 border" style="text-align: center;">
-                <h1>Atras</h1>
+            <div class = "col-md-12 " style="text-align: center;">
+                <a href="../index.php"class="btn no-icon">Atras</a>
             </div>
-            <div class = "col-md-12 border" style="text-align: center;">
-                <h1>Log out</h1>
+            <div class = "col-md-12 " style="text-align: center;">
+            <a href="#" class="btn no-icon">Log out</a>
             </div>
         </div>
 
         <div class="row" style="height:200px;">
-        <div class = "col-md-24 border" style="text-align: center;">
-            <h1>Eliminar</h1>
+        <div class = "col-md-24 " style="text-align: center; border-style:groove;">
+            <h1>Danger zone</h1>
+            <a href="# u sure?" class="btn btn-special no-icon" style="border-radius: 5px;">Borrar Cuenta</a>
+            <p> borrar tu cuenta implica que no podrás volver a logearte nunca más.</p>
         </div>
         </div>
     </div>
