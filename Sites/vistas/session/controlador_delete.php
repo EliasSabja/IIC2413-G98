564be6ee51629876e573eb229783912de5c374f6
Luckbox314 <50024324+Luckbox314@users.noreply.github.com>
2020-05-25 16:08:40 -0400

    Diseño de perfil

--- Sites/vistas/session/controlador_delete.php
+++ Sites/vistas/session/controlador_delete.php
@@ -2,7 +2,7 @@
     session_start();
     require("../../assets/conexion.php");
     $correo = $_SESSION["correo"];
-    $query_c = "UPDATE usuarios SET logueable=false WHERE correo = $correo;";
+    $query_c = "UPDATE usuarios SET logueable=false WHERE correo = '$correo';";
     $result_c = $db9 -> prepare($query_c);
     $result_c -> execute();
     $datos = $result_c -> fetchAll();
