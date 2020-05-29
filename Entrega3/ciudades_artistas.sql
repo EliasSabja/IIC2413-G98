1867baf9c054c834d0eb24c074f6951a0e0f3834
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-29 15:59:05 -0400

    ciudades_artistas

--- Entrega3/ciudades_artistas.sql
+++ Entrega3/ciudades_artistas.sql
@@ -5,7 +5,7 @@ DECLARE
     _aid INT;
 BEGIN
 CREATE TEMP TABLE cities(ciudad VARCHAR(255));
-FOREACH _aid SLICE 1 IN ARRAY _artistas LOOP
+FOREACH _aid IN ARRAY _artistas LOOP
     SELECT * FROM cities
     UNION
     SELECT DISTINCT C.ciudad AS ciudad FROM Pinto AS P, Obras AS O, Lugares AS L, Ciudades AS C WHERE _aid = P.aid AND P.oid = O.oid AND O.lid = L.lid AND L.cid = C.cid;
