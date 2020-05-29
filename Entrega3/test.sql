62a44e99a6a04b7c2e34aa89ca3397ef4c87680c
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-29 15:16:09 -0400

    Subir test procedimiento alma

--- Entrega3/test.sql
+++ Entrega3/test.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION ciudades_artista (aid INT)
-RETURNS TABLE(ciudad VARCHAR(255) AS
+RETURNS TABLE(ciudad VARCHAR(255)) AS
 $$
 BEGIN
 RETURN QUERY SELECT DISTINCT C.ciudad FROM Pinto AS P, Obras AS O, Lugares AS L, Ciudades AS C WHERE INPUT_AID = P.aid AND P.oid = O.oid AND O.lid = L.lid AND L.cid = C.cid;
