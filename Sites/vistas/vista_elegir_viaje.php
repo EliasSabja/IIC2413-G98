bb5b5b9678b0b825d9ec367c499040c8d3dec0c8
Luckbox314 <50024324+Luckbox314@users.noreply.github.com>
2020-05-29 03:27:15 -0400

    debug

--- Sites/vistas/vista_elegir_viaje.php
+++ Sites/vistas/vista_elegir_viaje.php
@@ -5,10 +5,15 @@
 
 <?php
     require("../assets/conexion.php");
+
+    $ciudad_o=$_POST["ciudad_origen"];
+    $ciudad_d=$_POST["ciudad_destino"];
+    $date=$_POST["date"];
+
     $query = "SELECT DISTINCT V.vid, V.precio, V.medio, V.h_salida, V.duracion FROM Viajes AS V, VOD WHERE V.vid = VOD.vid AND :origen= VOD.o_cid AND :destino = VOD.d_cid;";
     $result = $db9 -> prepare($query);
-    $result -> bindParam(':origen', $_POST["ciudad_origen"], PDO::PARAM_STR);
-    $result -> bindParam(':destino', $_POST["ciudad_destino"], PDO::PARAM_STR);
+    $result -> bindParam(':origen', $ciudad_o, PDO::PARAM_STR);
+    $result -> bindParam(':destino', $ciudad_d, PDO::PARAM_STR);
     $result -> execute();
     $data = $result -> fetchAll();
 ?>
@@ -24,11 +29,12 @@
             <article>
             <table class="custom">
             <tr>
-                <th>Precio</th><th>Medio</th><th>Hora de salida</th><th>Duración</th><th></th>
+                <th>vid</th><th>Precio</th><th>Medio</th><th>Hora de salida</th><th>Duración</th><th></th>
             </tr>
             <?php
                 foreach ($data as $p) {
                     echo "<tr>
+                    <td>$p[0]</td>
                     <td>$p[1]</td>
                     <td>$p[2]</td>
                     <td>$p[3]</td>
