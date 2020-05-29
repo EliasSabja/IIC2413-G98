54c9c67bb81e5917a91d52335a42dbd1991373bb
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-29 16:09:14 -0400

    ciudades_artistas

--- Entrega3/ciudades_artistas.sql
+++ Entrega3/ciudades_artistas.sql
@@ -4,14 +4,9 @@ $$
 DECLARE
     _aid INT;
 BEGIN
-CREATE TEMP TABLE cities(ciudad VARCHAR(255));
 FOREACH _aid IN ARRAY _artistas LOOP
-    SELECT * FROM cities
-    UNION
-    SELECT DISTINCT C.ciudad AS ciudad FROM Pinto AS P, Obras AS O, Lugares AS L, Ciudades AS C WHERE _aid = P.aid AND P.oid = O.oid AND O.lid = L.lid AND L.cid = C.cid;
+    RETURN QUERY (SELECT DISTINCT C.ciudad AS ciudad FROM Pinto AS P, Obras AS O, Lugares AS L, Ciudades AS C WHERE _aid = P.aid AND P.oid = O.oid AND O.lid = L.lid AND L.cid = C.cid);
 END LOOP;
-RETURN(SELECT * FROM cities);
 RETURN;
-DROP TABLE cities;
 END;
 $$ LANGUAGE plpgsql;
