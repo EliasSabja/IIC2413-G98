95d120dae5046f61eabddc5e9540ca6906b585b7
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-30 18:45:48 -0400

    TEST ITINERARIO

--- Entrega3/itinerario.sql
+++ Entrega3/itinerario.sql
@@ -9,10 +9,13 @@ DECLARE
     _precio_total FLOAT;
     _itinerario INT;
     _datos RECORD;
+    _aid INT;
 BEGIN
     DROP TABLE IF EXISTS c_visitables;
     CREATE TEMPORARY TABLE c_visitables (ciudad VARCHAR(255));
-    INSERT INTO c_visitables(ciudad) SELECT ciudad FROM (ciudades_artistas(_artistas));
+    FOREACH _aid IN ARRAY _artistas LOOP
+        SELECT * INTO c_visitables FROM (SELECT DISTINCT C.ciudad AS ciudad FROM Pinto AS P, Obras AS O, Lugares AS L, Ciudades AS C WHERE _aid = P.aid AND P.oid = O.oid AND O.lid = L.lid AND L.cid = C.cid);
+    END LOOP;
     
     EXEC SQL CONNECT TO :grupo99e2 USER :grupo99;
 
