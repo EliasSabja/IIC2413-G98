d75321f0221a2f308494b31767f800d1c09c5b96
Luckbox314 <50024324+Luckbox314@users.noreply.github.com>
2020-05-30 16:12:40 -0400

    fixed back button in vista_ticket

--- Sites/vistas/vista_ticket.php
+++ Sites/vistas/vista_ticket.php
@@ -81,7 +81,8 @@
                 <div class = "col-md-6 col " style="text-align: center;padding:20px;"></div>
 
                     <!-- Button -->
-                    <form action='vista_elegir_viaje'>
+                    <form action='vista_elegir_viaje.php
+                    '>
                     <?php
                     $ciudad_o = $_GET["ciudad_origen"];
                     $ciudad_d = $_GET["ciudad_origen"];                    
