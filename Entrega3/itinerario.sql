36ba275616289f129d90a585ef3528dac652153c
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-31 16:05:56 -0400

    nueva consulta

--- Entrega3/itinerario.sql
+++ Entrega3/itinerario.sql
@@ -1,16 +1,19 @@
 CREATE OR REPLACE FUNCTION itinerario (_artistas INT[], _fecha DATE, _origen INT)
-RETURNS TABLE(itinerario INT, precio_total FLOAT, fecha DATE, origen VARCHAR(255), destino VARCHAR(255), medio VARCHAR(255), hora_salida TIME, duracion INT, precio FLOAT) AS
+RETURNS TABLE(itinerario INT, precio_total FLOAT, fecha DATE, origen VARCHAR(255), destino VARCHAR(255), medio VARCHAR(255), h_salida TIME, duracion INT, precio FLOAT) AS
 $$
 DECLARE
-    _ciudad RECORD;
+    _fecha_actual DATE;
+    _precio_total FLOAT;
     _v1 RECORD;
     _v2 RECORD;
     _v3 RECORD;
-    _precio_total FLOAT;
     _itinerario INT;
-    _datos RECORD;
+    _datos1 RECORD;
+    _datos2 RECORD;
+    _datos3 RECORD;
     _aid INT;
-    _c_origen RECORD;
+    _duracion INTERVAL;
+    _dia INT;
 BEGIN
     DROP TABLE IF EXISTS c_visitables;
     CREATE TEMPORARY TABLE c_visitables (ciudad VARCHAR(255));
@@ -18,42 +21,58 @@ BEGIN
         INSERT INTO c_visitables (ciudad) SELECT DISTINCT C.ciudad AS ciudad FROM Pinto AS P, Obras AS O, Lugares AS L, Ciudades AS C WHERE _aid = P.aid AND P.oid = O.oid AND O.lid = L.lid AND L.cid = C.cid;
     END LOOP;
 
-    precio_total := 0;
-    itinerario := 1;
+    _precio_total := 0;
+    _itinerario := 1;
+    _dia := 1;
     DROP TABLE IF EXISTS I;
-    CREATE TEMPORARY TABLE I (itinerario INT, origen VARCHAR(255), destino VARCHAR(255), medio VARCHAR(255), hora_salida TIME, duracion INT, precio FLOAT);
-    FOR _v1 IN (SELECT * FROM VOD WHERE o_cid = _origen AND d_cid IN (SELECT DISTINCT C.cid FROM (SELECT * FROM dblink('dbname=grupo98e2 user=grupo98 password=perro123', 'SELECT * FROM c_visitables') AS FOO) AS CV, Ciudades AS C WHERE CV.nombre = C.nombre)) LOOP
-        CASE WHEN EXISTS (SELECT * FROM VOD WHERE _v1.d_cid = o_cid AND _v1.o_cid <> d_cid AND d_cid IN (SELECT DISTINCT C.cid FROM (SELECT * FROM dblink('dbname=grupo98e2 user=grupo98 password=perro123', 'SELECT * FROM c_visitables') AS FOO) AS CV, Ciudades AS C WHERE CV.nombre = C.nombre)) THEN
-            FOR _v2 IN (SELECT * FROM VOD WHERE _v1.d_cid = o_cid AND _v1.o_cid <> d_cid AND d_cid IN (SELECT DISTINCT C.cid FROM (SELECT * FROM dblink('dbname=grupo98e2 user=grupo98 password=perro123', 'SELECT * FROM c_visitables') AS FOO) AS CV, Ciudades AS C WHERE CV.nombre = C.nombre)) LOOP
-                CASE WHEN EXISTS (SELECT * FROM VOD WHERE _v2.d_cid = o_cid AND _v2.o_cid <> d_cid AND _v1.o_cid <> d_cid AND d_cid IN (SELECT DISTINCT C.cid FROM (SELECT * FROM dblink('dbname=grupo98e2 user=grupo98 password=perro123', 'SELECT * FROM c_visitables') AS FOO) AS CV, Ciudades AS C WHERE CV.nombre = C.nombre)) THEN
-                    FOR _v3 IN (SELECT * FROM VOD WHERE _v2.d_cid = o_cid AND _v2.o_cid <> d_cid AND _v1.o_cid <> d_cid AND d_cid IN (SELECT DISTINCT C.cid FROM (SELECT * FROM dblink('dbname=grupo98e2 user=grupo98 password=perro123', 'SELECT * FROM c_visitables') AS FOO) AS CV, Ciudades AS C WHERE CV.nombre = C.nombre)) LOOP
-                        FOR _datos IN (SELECT C1.ciudad AS origen1, C2.ciudad AS destino1, V.medio AS medio1, V.hora_salida AS hora1, V.duracion AS duracion1, V.precio AS precio1 FROM Viajes AS V, VDO, Ciudades AS C1, Ciudades AS C2 WHERE V.vid = _v1.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD_d_cid = C2.cid) LOOP
-                            INSERT INTO I VALUES(itinerario, _datos.origen1, _datos.destino1, _datos.medio1, _datos.hora1, _datos.duracion1, _datos.precio1);
-                        END LOOP;
-                        FOR _datos IN (SELECT C1.ciudad AS origen2, C2.ciudad AS destino2, V.medio AS medio2, V.hora_salida AS hora2, V.duracion AS duracion2, V.precio AS precio2 FROM Viajes AS V, VDO, Ciudades AS C1, Ciudades AS C2 WHERE V.vid = _v2.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD_d_cid = C2.cid) LOOP
-                            INSERT INTO I VALUES(itinerario, _datos.origen2, _datos.destino2, _datos.medio2, _datos.hora2, _datos.duracion2, _datos.precio2);
-                        END LOOP;
-                        FOR _datos IN (SELECT C1.ciudad AS origen3, C2.ciudad AS destino3, V.medio AS medio3, V.hora_salida AS hora3, V.duracion AS duracion3, V.precio AS precio3 FROM Viajes AS V, VDO, Ciudades AS C1, Ciudades AS C2 WHERE V.vid = _v3.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD_d_cid = C2.cid) LOOP
-                            INSERT INTO I VALUES(itinerario, _datos.origen3, _datos.destino3, _datos.medio3, _datos.hora3, _datos.duracion3, _datos.precio3);
-                            itinerario = itinerario + 1;
-                        END LOOP;
+    CREATE TEMPORARY TABLE I (itinerario INT, precio_total FLOAT, fecha DATE, origen VARCHAR(255), destino VARCHAR(255), medio VARCHAR(255), h_salida TIME, duracion INT, precio FLOAT);
+    FOR _v1 IN (SELECT * FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD WHERE o_cid = _origen AND d_cid IN (SELECT DISTINCT C.cid FROM c_visitables AS CV, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C WHERE CV.ciudad = C.ciudad)) LOOP
+        CASE WHEN EXISTS (SELECT * FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD WHERE _v1.d_cid = o_cid AND _v1.o_cid <> d_cid AND d_cid IN (SELECT DISTINCT C.cid FROM c_visitables AS CV, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C WHERE CV.ciudad = C.ciudad)) THEN
+            FOR _v2 IN (SELECT * FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD WHERE _v1.d_cid = o_cid AND _v1.o_cid <> d_cid AND d_cid IN (SELECT DISTINCT C.cid FROM c_visitables AS CV, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C WHERE CV.ciudad = C.ciudad)) LOOP
+                CASE WHEN EXISTS (SELECT * FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD WHERE _v2.d_cid = o_cid AND _v2.o_cid <> d_cid AND _v1.o_cid <> d_cid AND d_cid IN (SELECT DISTINCT C.cid FROM c_visitables AS CV, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C WHERE CV.ciudad = C.ciudad)) THEN
+                    FOR _v3 IN (SELECT * FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD WHERE _v2.d_cid = o_cid AND _v2.o_cid <> d_cid AND _v1.o_cid <> d_cid AND d_cid IN (SELECT DISTINCT C.cid FROM c_visitables AS CV, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C WHERE CV.ciudad = C.ciudad)) LOOP
+                        SELECT C1.ciudad AS origen1, C2.ciudad AS destino1, V.medio AS medio1, V.h_salida AS hora1, V.duracion AS duracion1, V.precio AS precio1 FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Viajes') AS V(vid INT, h_salida TIME, duracion INT, medio VARCHAR(50), capacidad INT, precio FLOAT)) AS V, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C1(cid INT, ciudad VARCHAR(50))) AS C1, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C2 WHERE V.vid = _v1.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD.d_cid = C2.cid INTO _datos1;
+                        SELECT C1.ciudad AS origen2, C2.ciudad AS destino2, V.medio AS medio2, V.h_salida AS hora2, V.duracion AS duracion2, V.precio AS precio2 FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Viajes') AS V(vid INT, h_salida TIME, duracion INT, medio VARCHAR(50), capacidad INT, precio FLOAT)) AS V, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C1(cid INT, ciudad VARCHAR(50))) AS C1, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C2 WHERE V.vid = _v2.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD.d_cid = C2.cid INTO _datos2;
+                        SELECT C1.ciudad AS origen3, C2.ciudad AS destino3, V.medio AS medio3, V.h_salida AS hora3, V.duracion AS duracion3, V.precio AS precio3 FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Viajes') AS V(vid INT, h_salida TIME, duracion INT, medio VARCHAR(50), capacidad INT, precio FLOAT)) AS V, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C1(cid INT, ciudad VARCHAR(50))) AS C1, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C2 WHERE V.vid = _v3.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD.d_cid = C2.cid INTO _datos3;
+                        _precio_total := _datos1.precio1 + _datos2.precio2 + _datos3.precio3;
+                        INSERT INTO I VALUES(_itinerario, _precio_total, _fecha, _datos1.origen1, _datos1.destino1, _datos1.medio1, _datos1.hora1, _datos1.duracion1, _datos1.precio1);
+                        _fecha_actual := _fecha;
+                        _duracion := make_interval(hours => _datos1.duracion1);
+                        IF _datos1.hora1 + _duracion > _datos2.hora2 THEN
+                            _fecha_actual := _fecha_actual + _dia;
+                        ELSE NULL;
+                        END IF;
+                        INSERT INTO I VALUES(_itinerario, _precio_total, _fecha_actual, _datos2.origen2, _datos2.destino2, _datos2.medio2, _datos2.hora2, _datos2.duracion2, _datos2.precio2);
+                        _duracion := make_interval(hours => _datos2.duracion2);
+                        IF _datos2.hora2 + _duracion > _datos3.hora3 THEN
+                            _fecha_actual := _fecha_actual + _dia;
+                        ELSE NULL;
+                        END IF;
+                        INSERT INTO I VALUES(_itinerario, _precio_total, _fecha_actual, _datos3.origen3, _datos3.destino3, _datos3.medio3, _datos3.hora3, _datos3.duracion3, _datos3.precio3);
+                        _itinerario := _itinerario + 1;
                     END LOOP;
-                ElSE
-                    FOR _datos IN (SELECT C1.ciudad AS origen2, C2.ciudad AS destino2, V.medio AS medio2, V.hora_salida AS hora2, V.duracion AS duracion2, V.precio AS precio2 FROM Viajes AS V, VDO, Ciudades AS C1, Ciudades AS C2 WHERE V.vid = _v2.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD_d_cid = C2.cid) LOOP
-                        INSERT INTO I VALUES(itinerario, _datos.origen2, _datos.destino2, _datos.medio2, _datos.hora2, _datos.duracion2, _datos.precio2);
-                        itinerario = itinerario + 1;
-                    END LOOP;
+                ELSE NULL;              
                 END CASE;
+                SELECT C1.ciudad AS origen1, C2.ciudad AS destino1, V.medio AS medio1, V.h_salida AS hora1, V.duracion AS duracion1, V.precio AS precio1 FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Viajes') AS V(vid INT, h_salida TIME, duracion INT, medio VARCHAR(50), capacidad INT, precio FLOAT)) AS V, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C1(cid INT, ciudad VARCHAR(50))) AS C1, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C2 WHERE V.vid = _v1.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD.d_cid = C2.cid INTO _datos1;
+                SELECT C1.ciudad AS origen2, C2.ciudad AS destino2, V.medio AS medio2, V.h_salida AS hora2, V.duracion AS duracion2, V.precio AS precio2 FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Viajes') AS V(vid INT, h_salida TIME, duracion INT, medio VARCHAR(50), capacidad INT, precio FLOAT)) AS V, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C1(cid INT, ciudad VARCHAR(50))) AS C1, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C2 WHERE V.vid = _v2.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD.d_cid = C2.cid INTO _datos2;
+                _precio_total := _datos1.precio1 + _datos2.precio2;
+                INSERT INTO I VALUES(_itinerario, _precio_total, _fecha, _datos1.origen1, _datos1.destino1, _datos1.medio1, _datos1.hora1, _datos1.duracion1, _datos1.precio1);
+                _fecha_actual := _fecha;
+                _duracion := make_interval(hours => _datos1.duracion1);
+                IF _datos1.hora1 + _duracion > _datos2.hora2 THEN
+                    _fecha_actual := _fecha_actual + _dia;
+                ELSE NULL;
+                END IF;
+                INSERT INTO I VALUES(_itinerario, _precio_total, _fecha_actual, _datos2.origen2, _datos2.destino2, _datos2.medio2, _datos2.hora2, _datos2.duracion2, _datos2.precio2);
+                _itinerario := _itinerario + 1;
             END LOOP;
-        ElSE
-            FOR _datos IN (SELECT C1.ciudad AS origen1, C2.ciudad AS destino1, V.medio AS medio1, V.hora_salida AS hora1, V.duracion AS duracion1, V.precio AS precio1 FROM Viajes AS V, VDO, Ciudades AS C1, Ciudades AS C2 WHERE V.vid = _v1.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD_d_cid = C2.cid) LOOP
-                INSERT INTO I VALUES(itinerario, _datos.origen1, _datos.destino1, _datos.medio1, _datos.hora1, _datos.duracion1, _datos.precio1);
-                itinerario = itinerario + 1;
-            END LOOP;
+        ELSE NULL;
         END CASE;
+        SELECT C1.ciudad AS origen1, C2.ciudad AS destino1, V.medio AS medio1, V.h_salida AS hora1, V.duracion AS duracion1, V.precio AS precio1 FROM (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Viajes') AS V(vid INT, h_salida TIME, duracion INT, medio VARCHAR(50), capacidad INT, precio FLOAT)) AS V, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM VOD') AS VOD(vid INT, o_cid INT, d_cid INT)) AS VOD, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C1(cid INT, ciudad VARCHAR(50))) AS C1, (SELECT * FROM dblink('dbname=grupo99e2 user=grupo99 password=Soto314', 'SELECT * FROM Ciudades') AS C(cid INT, ciudad VARCHAR(50))) AS C2 WHERE V.vid = _v1.vid AND V.vid = VOD.vid AND VOD.o_cid = C1.cid AND VOD.d_cid = C2.cid INTO _datos1;
+        INSERT INTO I VALUES(_itinerario, _datos1.precio1, _fecha,_datos1.origen1, _datos1.destino1, _datos1.medio1, _datos1.hora1, _datos1.duracion1, _datos1.precio1);
+        _itinerario := _itinerario + 1;
     END LOOP;
-
-    RETURN QUERY SELECT * FROM I;
+    RETURN QUERY (SELECT * FROM I ORDER BY precio_total, itinerario, fecha, h_salida);
 RETURN;
 END;
 $$ LANGUAGE plpgsql;
