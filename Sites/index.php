bef8c0c761b799bb0e248dbed4acfd035e27c710
Luckbox314 <50024324+Luckbox314@users.noreply.github.com>
2020-05-31 16:47:36 -0400

    1

--- Sites/index.php
+++ Sites/index.php
@@ -66,7 +66,7 @@
                </div>
                
                <div class = "col-md-24 " style="text-align: center;padding:20px;">
-                  <a href="vistas/vista_itinerario.php"class="btn btn-special no-icon"style="margin:0 auto;border-radius: 5px;">Crea tu itinerario</a>
+                  <a href="vistas/vista_itinerario.php"class="btn btn-special no-icon"style="margin:0 auto;border-radius: 5px;">Crear itinerario</a>
                </div>
 
                <!--botones-->
@@ -202,18 +202,16 @@
                   </div>
                </div>
 
-               <!--
                <div class="col-md-5">
                   <div class="widget widget_links">
-                     <h5 class="widget-title">Comprar</h5>
+                     <h5 class="widget-title">Viajes y estadía</h5>
                      <ul>
-                        <li><a href="#">Ticket</a></li>
-                        <li><a href="#">Reserva</a></li>
-                        <li><a href="#">Entrada</a></li>
+                        <li><a href="vistas/vista_comprar_ticket.php">Comprar ticket de viaje</a></li>
+                        <li><a href="vistas/vista_hacer_reserva.php">Hacer reserva de hotel</a></li>
+                        <li><a href="vistas/vista_itinerario.php">Crear itinerario</a></li>
                      </ul>
                   </div>
                </div>
-               -->
 
                
                <div class="col-md-5">
--- Sites/vistas/itinerario_controller_test.php
+++ Sites/vistas/itinerario_controller_test.php
@@ -1,9 +1,7 @@
 <?php $aids = $_POST['artistas_aid'];
 
-    echo "$aids";
-
     for($i=0; $i < count($aids); $i++){
-        echo "Selected " . $aids[$i] . "<br/>";
+        echo "Selected " . $aids[$i] . "<br>";
     }
     echo "<br>";
     foreach ($aids as $p) {
