4fb72cc01b8cec1009cd6e349675884d0c32e823
Diego Bustamante <diegobustama@uc.cl>
2020-07-10 17:00:37 -0400

    sol

--- Sites/vistas/msg_recibidos.php
+++ Sites/vistas/msg_recibidos.php
@@ -6,14 +6,16 @@
     $id = $_SESSION["id"];
     $response = file_get_contents("http://go-art.herokuapp.com/messages/$id");
     $resp = substr($response, 1, -2);
+    echo $
+    $respuestas = explode(",", $pizza);
     $resp = json_decode($resp);
     echo $resp->{'message'};
     #$msgs = array();
-    foreach($response as $resp){
-        $resp = json_decode($resp);
-        echo $resp->{'message'};
-        #$msgs
-    }
+    #foreach($response as $resp){
+    #    $resp = json_decode($resp);
+    #    echo $resp->{'message'};
+    #    $msgs
+    #}
 ?>
 
 
