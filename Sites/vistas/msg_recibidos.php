b60c6f7b6a22ec6508002a4182c765b18f112278
Diego Bustamante <diegobustama@uc.cl>
2020-07-10 13:18:32 -0400

    test api

--- Sites/vistas/msg_recibidos.php
+++ Sites/vistas/msg_recibidos.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
     include('../templates/header.html');
+?>
+<?php
     $id = $_SESSION["id"];
     $response = file_get_contents("http://go-art.herokuapp.com/messages/$id");
     $response = json_decode($response);
