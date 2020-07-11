f5544b50a545d9978363ff7d4821b3e077c5280a
EliasSabja <elias.sabja@uc.cl>
2020-07-11 19:08:10 -0400

    mapa test

--- Sites/vistas/msg_mapa.php
+++ Sites/vistas/msg_mapa.php
@@ -6,15 +6,6 @@
     $id = $_SESSION["id"];
     $fecha_i = $_POST["date_i"];
     $fecha_f = $_POST["date_f"];
-    $response = file_get_contents("http://go-art.herokuapp.com/messages?id1=$id");
-    $response = str_replace("},{", "}~~{", $response);
-    $response = substr($response, 1, -2);
-    $respuestas = explode("~~", $response);
-    $msgs = array();
-    foreach($respuestas as $resp){
-        $resp = json_decode($resp);
-        array_push($msgs, $resp);
-    }
 ?>
 
 <section class="section section-destination">
@@ -24,18 +15,7 @@
         </div>
     </div>
     
-    <div class="mapid">
-        <script>
-            var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoiZWxpYXMyMTA1c2IiLCJhIjoiY2tjaWhteHVqMHFidzJxbzBwMmE0MGUwbCJ9.9K8F9xdOATN10tNQjADDTQ'}).addTo(mymap);
-            var marker = L.marker([51.5, -0.09]).addTo(mymap);
-        </script>
-    </div>
+    <div id="mapid"></div>
+    
 </section>
 <?php include('../templates/footer.html'); ?>
