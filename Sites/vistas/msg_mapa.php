e6c82328b01b733f4f095ab8709bbadb948d8586
EliasSabja <elias.sabja@uc.cl>
2020-07-11 19:26:26 -0400

    mapa test 6

--- Sites/vistas/msg_mapa.php
+++ Sites/vistas/msg_mapa.php
@@ -6,6 +6,37 @@
     $id = $_SESSION["id"];
     $fecha_i = $_POST["date_i"];
     $fecha_f = $_POST["date_f"];
+    $response = file_get_contents("http://go-art.herokuapp.com/messages?id1=$id");
+    $response = str_replace("},{", "}~~{", $response);
+    $response = substr($response, 1, -2);
+    $respuestas = explode("~~", $response);
+    $msgs = array();
+    foreach($respuestas as $resp){
+        $resp = json_decode($resp);
+        array_push($msgs, $resp);
+    }
+
+    echo 'mensajes', $msgs;
+
+    foreach ($msgs as $m) {
+        if (
+            strtotime($m->{'date'}) > strtotime($fecha_i) &
+            strtotime($m->{'date'}) < strtotime($fecha_f)
+        ){
+            $f = $m->{'date'};
+            $la = $m->{'lat'};
+            $lo = $m->{'long'};
+            $me = $m->{'message'};
+            $mid = $m->{'mid'};
+            $r = $m->{'receptant'};
+            $s = $m->{'sender'};
+            echo "<tr><td>$f</td><td>$la</td><td>$lo</td><td>$me</td><td>$mid</td><td>$r</td><td>$s</td></tr>";
+        }
+        
+
+    }
+
+    #echo "<tr><td>$fecha_i</td><td>$fecha_f</td></tr>";
 ?>
 
 <section class="section section-destination">
