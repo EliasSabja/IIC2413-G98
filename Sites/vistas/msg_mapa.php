e45432e7d56675b15284d7f41bcffbef2a423341
EliasSabja <elias.sabja@uc.cl>
2020-07-11 18:53:34 -0400

    mapita 3

--- Sites/vistas/msg_mapa.php
+++ Sites/vistas/msg_mapa.php
@@ -25,17 +25,17 @@
     </div>
     
     <div class="mapid">
-    <script>
-        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoiZWxpYXMyMTA1c2IiLCJhIjoiY2p1aHQ0bG5qMTNmeDQ0dTlrYmVwczIxeiJ9.nQgtI9N87X-eoI5ROjoh4A'}).addTo(mymap);
-        var marker = L.marker([51.5, -0.09]).addTo(mymap);
-    </script>
+        <script>
+            var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiZWxpYXMyMTA1c2IiLCJhIjoiY2p1aHQ0bG5qMTNmeDQ0dTlrYmVwczIxeiJ9.nQgtI9N87X-eoI5ROjoh4A'}).addTo(mymap);
+            var marker = L.marker([51.5, -0.09]).addTo(mymap);
+        </script>
     </div>
 </section>
 <?php include('../templates/footer.html'); ?>
