0abcf2f8a5ffb7492c8f7ba841665fb682a2a2c5
EliasSabja <elias.sabja@uc.cl>
2020-07-11 20:31:01 -0400

    mapa test 21 debug 7

--- Sites/vistas/msg_mapa.php
+++ Sites/vistas/msg_mapa.php
@@ -99,10 +99,10 @@ crossorigin=""></script>
             $f_i = new DateTime($fecha_i);
             $f_f = new DateTime($fecha_i);
             $date = new DateTime($msg->{"date"});
-            echo 'console.log(' . strval($f_i) . ');';
-            echo 'console.log(' . strval($f_f) . ');';
-            echo 'console.log(' . strval($date) . ');';
-            if(true){
+            echo 'console.log(' . strval($f_i->format("Y-m-d")) . ');';
+            echo 'console.log(' . strval($f_f->format("Y-m-d")) . ');';
+            echo 'console.log(' . strval($date->format("Y-m-d")) . ');';
+            if($date > $f_i && $date < $f_f){
                 echo "L.marker([" . strval($msg->{"lat"}) . "," . strval($msg->{"long"}) . "]).addTo(mymap).bindPopup('<b>" . strval($msg->{"message"}) . "</b>');";
             }
         }
