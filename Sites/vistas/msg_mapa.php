7b9e90fe70a560c79c46116c06bcfcdf8fc1e57e
Luckbox314 <50024324+Luckbox314@users.noreply.github.com>
2020-07-11 20:31:08 -0400

    fecha de mensajes

--- Sites/vistas/msg_mapa.php
+++ Sites/vistas/msg_mapa.php
@@ -48,7 +48,7 @@
                     echo "<tr><td>$f</td><td>$la</td><td>$lo</td><td>$me</td><td>$mid</td><td>$r</td><td>$s</td></tr>";
                 }
 
-                echo "<tr><td>$fecha_i</td><td>$fecha_f</td></tr>;
+                echo "<tr><td>$fecha_i</td><td>$fecha_f</td></tr>";
                
             ?> 
             </table>
